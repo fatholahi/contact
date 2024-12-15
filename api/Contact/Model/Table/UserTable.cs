e7fd77f9976be49c27237275158db96cca158eaf
fatholahi <fatholahi.mojtaba@gmail.com>
2024-12-15 10:55:24 +0330

    dapper sample codes

---
+++ api/Contact/Model/Table/UserTable.cs
@@ -0,0 +1,11 @@
+﻿namespace Contact.Model.Table
+{
+    public class UserTable
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] Password { get; set; }
+        public string FullName { get; set; }
+        public string Avatar { get; set; }
+    }
+}
---
+++ api/Contact/Program.cs
@@ -0,0 +1,54 @@
+﻿using Contact.Model.Table;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.ComponentModel;
+
+namespace Contact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=.;Database=Contacts;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
+
+            SqlConnection db = new SqlConnection(connectionString);
+
+            db.Open();
+
+            //string sql = "insert into dbo.[User] (Username, Password, FullName) values (@Username, @Password, @FullName)";
+
+            //UserTable user = new UserTable()
+            //{
+            //    Username = "admin",
+            //    Password = [],
+            //    FullName = "Administrator"
+            //};
+
+            //object param = new
+            //{
+            //    Username = "mojtaba",
+            //    Password = Array.Empty<byte>(),
+            //    FullName = "Mojtaba"
+            //};
+
+            //db.Execute(sql, param);
+
+            //string sql = "delete from dbo.[User] where Id <> @Id";
+
+            //int affectedRows = db.Execute(sql, new { Id = 1001 });
+
+            //Console.WriteLine(affectedRows);
+
+            string sql = "select UserName, FullName, 1 Age from dbo.[User] WHERE 1 > 1";
+
+            IEnumerable<UserTable> users = db.Query<UserTable>(sql);
+
+            foreach (UserTable user in users)
+            {
+                Console.WriteLine($"UserId: {user.Id}, Username: {user.Username}, Full Name: {user.FullName}");
+            }
+
+            db.Close();
+        }
+    }
+}
