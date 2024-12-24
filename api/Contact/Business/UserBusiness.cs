7bb8bb7412eb25188e1e1f14bdadeea6df0fc35e
fatholahi <fatholahi.mojtaba@gmail.com>
2024-12-24 12:21:24 +0330

    fix error for users without avatar

--- api/Contact/Business/UserBusiness.cs
+++ api/Contact/Business/UserBusiness.cs
@@ -10,95 +10,100 @@ using Contact.Model.User;
 
 namespace Contact.Business
 {
-    public class UserBusiness
-    {
-        public BusinessResult<int> RegisterBusiness(UserAddModel model)
-        {
-            BusinessResult<int> result = new();
+	public class UserBusiness
+	{
+		public BusinessResult<int> RegisterBusiness(UserAddModel model)
+		{
+			BusinessResult<int> result = new();
 
-            if (model.Password != model.PasswordConfirm)
-            {
-                result.Success = false;
-                result.ErrorCode = 1001;
-                result.ErrorMessage = "Password does not match";
+			if (model.Password != model.PasswordConfirm)
+			{
+				result.Success = false;
+				result.ErrorCode = 1001;
+				result.ErrorMessage = "Password does not match";
 
-                return result;
-            }
+				return result;
+			}
 
-            byte[] password = MD5.HashData(Encoding.UTF8.GetBytes(model.Password));
+			byte[] password = MD5.HashData(Encoding.UTF8.GetBytes(model.Password));
 
-            byte[] avatar = Convert.FromBase64String(model.ImageData);
+			byte[] avatar = Convert.FromBase64String(model.ImageData);
 
-            if (!Directory.Exists(@".\Avatar"))
-            {
-                Directory.CreateDirectory(@".\Avatar");
-            }
+			if (!Directory.Exists(@".\Avatar"))
+			{
+				Directory.CreateDirectory(@".\Avatar");
+			}
 
-            string file = @$".\Avatar\{model.Username.ToLower()}.png";
+			string file = @$".\Avatar\{model.Username.ToLower()}.png";
 
-            if (File.Exists(file))
-            {
-                File.Delete(file);
-            }
+			if (File.Exists(file))
+			{
+				File.Delete(file);
+			}
 
-            File.WriteAllBytes(file, avatar);
+			File.WriteAllBytes(file, avatar);
 
-            UserTable user = new()
-            {
-                Username = model.Username,
-                Password = password,
-                FullName = model.FullName,
-                Avatar = $"{model.Username.ToLower()}.png"
-            };
+			UserTable user = new()
+			{
+				Username = model.Username,
+				Password = password,
+				FullName = model.FullName,
+				Avatar = $"{model.Username.ToLower()}.png"
+			};
 
-            result.Data = new UserData().Insert(user);
+			result.Data = new UserData().Insert(user);
 
-            result.Success = true;
+			result.Success = true;
 
-            return result;
-        }
+			return result;
+		}
 
-        public BusinessResult<int> Login(UserLoginModel model)
-        {
-            byte[] password = MD5.HashData(Encoding.UTF8.GetBytes(model.Password));
+		public BusinessResult<int> Login(UserLoginModel model)
+		{
+			byte[] password = MD5.HashData(Encoding.UTF8.GetBytes(model.Password));
 
-            int id = new UserData().GetUserId(model.Username, password);
+			int id = new UserData().GetUserId(model.Username, password);
 
-            if (id == 0)
-            {
-                return new BusinessResult<int>()
-                {
-                    Success = false,
-                    ErrorCode = 2002,
-                    ErrorMessage = "Invalid username or password"
-                };
-            }
+			if (id == 0)
+			{
+				return new BusinessResult<int>()
+				{
+					Success = false,
+					ErrorCode = 2002,
+					ErrorMessage = "Invalid username or password"
+				};
+			}
 
-            return new BusinessResult<int>()
-            {
-                Success = true,
-                Data = id
-            };
-        }
+			return new BusinessResult<int>()
+			{
+				Success = true,
+				Data = id
+			};
+		}
 
-        public BusinessResult<UserProfileModel> ProfileBusiness(int userId)
-        {
-            UserTable table = new UserData().GetUserInfoById(userId);
+		public BusinessResult<UserProfileModel> ProfileBusiness(int userId)
+		{
+			UserTable table = new UserData().GetUserInfoById(userId);
 
-            string file = @$"C:\Users\Mojtaba\Desktop\contact\api\Contact\bin\Debug\net9.0\Avatar\{table.Username.ToLower()}.png";
+			string file = @$".\Avatar\{table.Username.ToLower()}.png";
 
-            string data = Convert.ToBase64String(File.ReadAllBytes(file));
+			string data = "";
 
-            return new BusinessResult<UserProfileModel>()
-            {
-                Success = true,
-                Data = new UserProfileModel()
-                {
-                    Avatar = data,
-                    Username = table.Username,
-                    FullName = table.FullName,
-                }
-            };
-        }
-    }
+			if (File.Exists(file))
+			{
+				data = Convert.ToBase64String(File.ReadAllBytes(file));
+			}
+
+			return new BusinessResult<UserProfileModel>()
+			{
+				Success = true,
+				Data = new UserProfileModel()
+				{
+					Avatar = data,
+					Username = table.Username,
+					FullName = table.FullName,
+				}
+			};
+		}
+	}
 }
