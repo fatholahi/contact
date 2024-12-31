d5dfbd2eb8fa6922ecd294038980d2405c65b5e8
fatholahi <fatholahi.mojtaba@gmail.com>
2024-12-31 14:05:24 +0330

    contact and phone empty components, routes and links

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './page/login-page/login-page.component';
 import { ProfilePageComponent } from './page/profile-page/profile-page.component';
 import { RegisterPageComponent } from './page/register-page/register-page.component';
+import { ContactPageComponent } from './page/contact-page/contact-page.component';
+import { PhonePageComponent } from './page/phone-page/phone-page.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginPageComponent },
     { path: 'profile', component: ProfilePageComponent },
     { path: 'register', component: RegisterPageComponent },
+    { path: 'contact', component: ContactPageComponent },
+    { path: 'phone', component: PhonePageComponent }
 ];
